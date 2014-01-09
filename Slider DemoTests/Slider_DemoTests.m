0bf14cfbd2b960575becaed606d48544fafd71bc
Jordan Hudgens <jordan@wow.com>
2014-01-08 21:21:24 -0600

    Initial Commit

---
+++ Slider DemoTests/Slider_DemoTests.m
@@ -0,0 +1,34 @@
+//
+//  Slider_DemoTests.m
+//  Slider DemoTests
+//
+//  Created by Jordan Hudgens on 1/8/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Slider_DemoTests : XCTestCase
+
+@end
+
+@implementation Slider_DemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
